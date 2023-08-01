ac97f27156fdc8c39d9de23183090631be181c36
eleeyeah <ilietmks@gmail.com>
2023-08-01 14:50:53 +0200

    added json file w/ fs

--- interview-backend/src/cities/cities.service.ts
+++ interview-backend/src/cities/cities.service.ts
@@ -1,24 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class CitiesService {
-  private cities = [
-    { id: 1, cityName: 'Paris', count: 300 },
-    { id: 2, cityName: 'London', count: 200 },
-    { id: 3, cityName: 'New York', count: 100 },
-    { id: 4, cityName: 'Berlin', count: 50 },
-    { id: 5, cityName: 'Tokyo', count: 30 },
-    { id: 6, cityName: 'Madrid', count: 20 },
-    { id: 7, cityName: 'Rome', count: 10 },
-  ];
+  private cities: any;
 
-  getCities(name_like: string) {
+  constructor() {
+    const filePath = path.resolve(__dirname, '../../../cities.json');
+    this.cities = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  }
+
+  getCities(name_like?: string): any {
     if (name_like) {
       return this.cities.filter((city) =>
         city.cityName.toLowerCase().includes(name_like.toLowerCase()),
       );
     } else {
-      return [];
+      return this.cities;
     }
   }
 }
