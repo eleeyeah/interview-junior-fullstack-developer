393ea2187517f4c2d20f5eb07a564f5e5e36ca08
eleeyeah <ilietmks@gmail.com>
2023-08-01 07:24:21 +0200

    enabled CORS

--- interview-backend/src/main.ts
+++ interview-backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET',
+    allowedHeaders: 'Content-Type, Accept', // The headers we want to allow
+  });
+
   await app.listen(3000);
 }
 bootstrap();
